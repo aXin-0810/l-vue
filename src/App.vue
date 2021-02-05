4ff429562bd6b9185f47a71b92e581b527e190ca
aXin-0810 <13750565823@163.com>
2021-02-05 15:32:16 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
   <div id="app">
-    成功
-    <aaa/>
+    开始疯狂编码
   </div>
 </template>
 
 <script>
-import aaa from "./aaa"
 export default {
-  components:{
-    aaa
-  },
   data(){
     return {
-      aaa:1
+    
     }
   },
   mounted() {
-    console.log(this.aaa)
+    
   },
 };
 </script>
 
-<style lang="css">
+<style scoped>
+*{
+  padding: 0 0;
+  margin: 0 0;
+  outline: none;
+}
+html,
+body, 
 #app{
   width: 100%;
   height: 100%;
-  background: brown;
+  color: brown;
+}
+/*修改滚动条样式*/
+*::-webkit-scrollbar {
+  width: 0px;
+  height: 0px;
+}
+*::-webkit-scrollbar-track {
+  background: rgba(0,0,0,0);
+  border-radius: 0px;
+}
+*::-webkit-scrollbar-thumb {
+  background: rgba(0,0,0,0);
+  border-radius: 0px;
+}
+*::-webkit-scrollbar-thumb:hover {
+  background: rgba(0,0,0,0);
+}
+*::-webkit-scrollbar-corner {
+  background: rgba(0,0,0,0);
 }
 </style>
